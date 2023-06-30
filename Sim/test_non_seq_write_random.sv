8505a4715c60361ca555afe6a52e77a775e92f9a
Ammar Khan <ammar.sarwar@10xengineers.ai>
2023-07-01 04:34:13 +0500

    htrans_nonseq_random_write test added

---
+++ Sim/test_non_seq_write_random.sv
@@ -0,0 +1,27 @@
+`include "amba_ahb_defines.sv"
+`include "environment.sv"
+
+program test(mem_intf vif);
+
+  class my_trans extends transaction;
+    function void pre_randomize();
+    HTRANS  .rand_mode(0);
+    HWRITE  .rand_mode(0);
+    HTRANS  = `H_NONSEQ;
+    HWRITE  = `H_WRITE;
+    endfunction  
+  endclass
+
+  environment env;
+  my_trans my_tr;
+
+  initial begin
+    env = new(vif);
+    my_tr = new();
+    env.gen.trans = my_tr;
+    env.gen.repeat_count = 25;
+    $readmemh("local_mem.txt", env.scb.local_memory,0,255); 
+    env.run();
+  end
+
+endprogram
--- Sim/testbench.sv
+++ Sim/testbench.sv
@@ -8,7 +8,8 @@
 //`include "test_htrans_busy.sv"
 // `include "test_non_seq_write_word.sv"
 // `include "test_non_seq_write_halfword.sv"
-`include "test_non_seq_write_byte.sv"
+// `include "test_non_seq_write_byte.sv"
+`include "test_non_seq_write_random.sv"
 //`include "test_write.sv"
 //`include "test_read.sv"
 
